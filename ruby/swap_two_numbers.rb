5dd7ac88b9d6f30e48f12a3fce4aedc1a58490cb
munishyadav08 <115063305+munishyadav08@users.noreply.github.com>
2022-10-15 20:21:39 -0700

    Update swap_two_numbers.rb
    added example program for swapping number 13 and 22

--- ruby/swap_two_numbers.rb
+++ ruby/swap_two_numbers.rb
@@ -18,3 +18,24 @@ end
 
 obj = SwapExample.new
 obj.swapNumbers
+
+class SwapExample
+    def swapNumbers
+        a = 13
+        b = 22
+
+        puts("The value of a before swap is " + a.to_s)
+        puts("The value of b before swap is " + b.to_s)
+
+        # Swapping of two numbers using arithmetic operations
+        a = a + b
+        b = a - b
+        a = a - b
+
+        puts("The value of a after swap is " + a.to_s)
+        puts("The value of b after swap is " + b.to_s)
+    end
+end
+
+obj = SwapExample.new
+obj.swapNumbers
