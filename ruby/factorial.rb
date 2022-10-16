f830ec095e3867d9717cd601b53b075d9e26f60f
munishyadav08 <115063305+munishyadav08@users.noreply.github.com>
2022-10-15 20:28:12 -0700

    Update factorial.rb
    added factorial program

--- ruby/factorial.rb
+++ ruby/factorial.rb
@@ -10,3 +10,18 @@ while i <= num  #implementation of while loop
 end
 
 puts "The factorial of #{num} is #{fact}"
+
+puts "Enter the number:"
+num=gets.chomp.to_i
+
+fact=1
+if (num==0)
+	puts "Error! Could not find the factorial of one"
+else
+	i=1
+	while(i<=num)
+		fact=fact*i
+		i+=1
+	end
+end
+		puts "factorial of #{num} is #{fact}"
