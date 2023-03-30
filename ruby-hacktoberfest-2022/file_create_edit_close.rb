15fc40a0251e5273e58b08bcda765aefb2c7f2e9
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-03-29 19:00:06 -0500

    Update file_create_edit_close.rb
    - Created a function to allow command line prompts to automate creation and editing of the file.

--- ruby-hacktoberfest-2022/file_create_edit_close.rb
+++ ruby-hacktoberfest-2022/file_create_edit_close.rb
@@ -1,14 +1,24 @@
 # This is a file handling (create, writing, and closing) program using Ruby
 
-=begin 
-First we will create a file which is assigned to a variable.
-This file will be created and added to the current project directory that this program is in
-=end
+def fileHandling
+  puts 'Please write the name of the file you want to create. Make sure and add the filetype at the end (e.g. .txt, .rb): '
+  name = gets.chomp.to_s
+  puts 'Please write what you are wanting to put into the file: '
+  toWrite = gets.chomp.to_s
+  # First we will create a file which is assigned to a variable.
+  # This file will be created and added to the current project directory that this program is in
+  # The name of the file will be what was entered at the first prompt.
+  fileObject = File.new(name, 'w+')
 
-fileObject = File.new('newFile.txt', 'w+')
+  # Next we are going to write to the file
+  # The content of the file will be what was input at the second prompt.
+  fileObject.syswrite(toWrite)
 
-# Next we are going to write to the file
-fileObject.syswrite('This is sample text being written to the newly created file.')
+  # Finally we will close the file that we created and wrote to
+  puts "The file you made and wrote to is located in the current directory! It's name is #{name}!"
+  fileObject.close
 
-# Finally we will close the file that we created and wrote to
-fileObject.close
+  # Now go and find the file you created, and see how you were able to to create, edit, and close a file with Ruby!
+end
+
+fileHandling
