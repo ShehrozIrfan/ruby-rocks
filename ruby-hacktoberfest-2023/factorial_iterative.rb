5a97e7a22319f9069deef6fa0c045aba594322d0
Devtanu Misra <67796502+devtanumisra@users.noreply.github.com>
2023-10-16 18:58:28 +0530

    Update factorial_iterative.rb

--- ruby-hacktoberfest-2023/factorial_iterative.rb
+++ ruby-hacktoberfest-2023/factorial_iterative.rb
@@ -12,3 +12,13 @@ def factorial_iterative(n)
     return result
   end
 end
+
+# Get user input
+print "Enter a non-negative integer to calculate its factorial: "
+num = gets.chomp.to_i
+
+if num < 0
+  puts "Factorial is not defined for negative numbers."
+else
+  puts "Iterative Factorial of #{num} is #{factorial_iterative(num)}"
+end
